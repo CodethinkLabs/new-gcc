404551283feca0f90dd51d33bd19bd492b7cbb48
Mark Eggleston <markeggleston@gcc.gnu.org>
2020-01-31 11:07:05 +0000

    Allow blank format items in format strings
    This has to be written in a slightly verbose manner because GCC 7
    defaults to building with -Werror=implicit-fallthrough which prevents
    us from just falling through to the default: case.
    
    Test written by: Francisco Redondo Marchena <francisco.marchena@codethink.co.uk>
    
    Use -fdec-blank-format-item to enable. Also enabled by -fdec.

---
+++ gcc/testsuite/gfortran.dg/dec_format_empty_item_3.f
@@ -0,0 +1,19 @@
+! { dg-do compile }
+! { dg-options "-fdec -fno-dec-blank-format-item" }
+!
+! Test blank/empty format items in format string
+!
+! Test case contributed by Jim MacArthur <jim.macarthur@codethink.co.uk>
+! Modified by Mark Eggleston <mark.eggleston@codethink.com>
+!
+        PROGRAM blank_format_items
+          INTEGER A/0/
+
+          OPEN(1, status="scratch")
+          WRITE(1, 10) 100 ! { dg-error "FORMAT label 10 at \\(1\\) not defined" }
+          REWIND(1)
+          READ(1, 10) A ! { dg-error "FORMAT label 10 at \\(1\\) not defined" }
+          IF (a.NE.100) STOP 1
+          PRINT 10, A ! { dg-error "FORMAT label 10 at \\(1\\) not defined" }
+10        FORMAT( I5,) ! { dg-error "Unexpected element" }
+        END
