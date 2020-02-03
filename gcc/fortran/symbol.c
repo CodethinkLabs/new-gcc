a8fb7f18582b293fa6f5f377e0992cab6eb7bf2e
Mark Eggleston <markeggleston@gcc.gnu.org>
2020-02-03 08:32:04 +0000

    Allow duplicate declarations.
    Enabled by -fdec-duplicates and -fdec.
    
    Some fixes by Jim MacArthur <jim.macarthur@codethink.co.uk>
    Addition of -fdec-duplicates by Mark Eggleston <mark.eggleston@codethink.com>

--- gcc/fortran/symbol.c
+++ gcc/fortran/symbol.c
@@ -1994,6 +1994,8 @@ gfc_add_type (gfc_symbol *sym, gfc_typespec *ts, locus *where)
   if (sym->attr.result && type == BT_UNKNOWN && sym->ns->proc_name)
     type = sym->ns->proc_name->ts.type;
 
+  flavor = sym->attr.flavor;
+
   if (type != BT_UNKNOWN && !(sym->attr.function && sym->attr.implicit_type)
       && !(gfc_state_stack->previous && gfc_state_stack->previous->previous
 	   && gfc_state_stack->previous->previous->state == COMP_SUBMODULE)
@@ -2003,9 +2005,26 @@ gfc_add_type (gfc_symbol *sym, gfc_typespec *ts, locus *where)
 	gfc_error ("Symbol %qs at %L conflicts with symbol from module %qs, "
 		   "use-associated at %L", sym->name, where, sym->module,
 		   &sym->declared_at);
+      else if (flag_dec_duplicates)
+	{
+	  /* Ignore temporaries and class/procedure names */
+	  if (sym->ts.type == BT_DERIVED || sym->ts.type == BT_CLASS
+	      || sym->ts.type == BT_PROCEDURE)
+	    return false;
+
+	  if (gfc_compare_types (&sym->ts, ts)
+	      && (flavor == FL_UNKNOWN || flavor == FL_VARIABLE
+	      || flavor == FL_PROCEDURE))
+	    {
+	      return gfc_notify_std (GFC_STD_LEGACY,
+				     "Symbol '%qs' at %L already has "
+				     "basic type of %s", sym->name, where,
+				     gfc_basic_typename (type));
+	    }
+	}
       else
 	gfc_error ("Symbol %qs at %L already has basic type of %s", sym->name,
-		 where, gfc_basic_typename (type));
+		   where, gfc_basic_typename (type));
       return false;
     }
 
@@ -2015,8 +2034,6 @@ gfc_add_type (gfc_symbol *sym, gfc_typespec *ts, locus *where)
 		 sym->name, where, gfc_basic_typename (ts->type));
       return false;
     }
-
-  flavor = sym->attr.flavor;
 
   if (flavor == FL_PROGRAM || flavor == FL_BLOCK_DATA || flavor == FL_MODULE
       || flavor == FL_LABEL
