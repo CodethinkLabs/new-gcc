15a096da67d55eb1029123d8928ac88b41581510
Mark Eggleston <markeggleston@gcc.gnu.org>
2020-04-08 15:35:20 +0100

    Suppress warning with -Wno-overwrite-recursive
    The message "Warning: Flag '-fno-automatic' overwrites '-frecursive'" is
    output by default when -fno-automatic and -frecursive are used together.
    It warns that recursion may be broken, however if all the relavent variables
    in the recursive procedure have automatic attributes the warning is
    unnecessary so -Wno-overwrite-recursive can be used to suppress it. This
    will allow compilation when warnings are regarded as errors.
    
    Suppress warning with -Wno-overwrite-recursive

--- gcc/fortran/options.c
+++ gcc/fortran/options.c
@@ -418,7 +418,7 @@ gfc_post_options (const char **pfilename)
       && flag_max_stack_var_size != 0)
     gfc_warning_now (0, "Flag %<-fno-automatic%> overwrites %<-fmax-stack-var-size=%d%>",
 		     flag_max_stack_var_size);
-  else if (!flag_automatic && flag_recursive)
+  else if (!flag_automatic && flag_recursive && warn_overwrite_recursive)
     gfc_warning_now (0, "Flag %<-fno-automatic%> overwrites %<-frecursive%>");
   else if (!flag_automatic && flag_openmp)
     gfc_warning_now (0, "Flag %<-fno-automatic%> overwrites %<-frecursive%> implied by "
